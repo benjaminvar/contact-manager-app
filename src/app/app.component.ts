4dcdfd78bc1cb02944de5032ea544ff4707de3a4
Benjamin Vargas <bvj2206@gmail.com>
2019-12-03 20:52:20 -0400

    remove data from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,6 @@
 import { Component } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { ContactAddFormComponent } from './contact-add-form/contact-add-form.component';
-
-export interface PeriodicElement {
-  name: string;
-  email: string;
-  phone: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  { name: 'Hydrogen', email: 'Hydrogen', phone: 'H'},
-  { name: 'Helium', email: 'Helium', phone: 'He'},
-  { name: 'Lithium', email: 'Lithium', phone: 'Li'},
-  { name: 'Beryllium', email: 'Beryllium', phone: 'Be'},
-  { name: 'Boron', email:'Boron', phone: 'B'},
-  { name: 'Carbon', email: 'Carbon', phone: 'C'},
-  { name: 'Nitrogen', email: 'Nitrogen', phone: 'N'},
-  { name: 'Oxygen', email: 'Oxygen', phone: 'O'},
-  { name: 'Fluorine', email: 'Fluorine', phone: 'F'},
-  { name: 'Neon', email: 'Neon', phone: 'Ne' },
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -27,7 +8,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class AppComponent {
   displayedColumns: string[] = ['name' ,'email','phone', 'actions'];
-  dataSource = ELEMENT_DATA;
+  dataSource;
   constructor(public dialog: MatDialog) {}
   showContactAddDialog()
   {
