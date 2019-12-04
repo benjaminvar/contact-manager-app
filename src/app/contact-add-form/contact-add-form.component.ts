bddd3178e3a7e1a6e7db81c72a18025b88c3fb9d
Benjamin Vargas <bvj2206@gmail.com>
2019-12-04 03:51:25 -0400

    add form validation

--- src/app/contact-add-form/contact-add-form.component.ts
+++ src/app/contact-add-form/contact-add-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject , OnInit } from '@angular/core';
 import { Contact } from "../contact"
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact-add-form',
   templateUrl: './contact-add-form.component.html',
@@ -9,9 +9,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ContactAddFormComponent implements OnInit {
   contactForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    phone: new FormControl('',[Validators.required, Validators.pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/)]),
   });
   constructor(public dialogRef: MatDialogRef<ContactAddFormComponent>, 
     @Inject(MAT_DIALOG_DATA) public data: Contact) { }
