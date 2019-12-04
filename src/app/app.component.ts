e72944b3e72fd0763d29e882862c1ec620390c04
Benjamin Vargas <bvj2206@gmail.com>
2019-12-03 20:55:40 -0400

    Add ContactService dependency to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { ContactAddFormComponent } from './contact-add-form/contact-add-form.component';
 import {MatTableDataSource} from "@angular/material"
+import { ContactService } from './contact.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +11,7 @@ import {MatTableDataSource} from "@angular/material"
 export class AppComponent {
   displayedColumns: string[] = ['name' ,'email','phone', 'actions'];
   dataSource = new MatTableDataSource();
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, public contactService: ContactService) {}
   showContactAddDialog()
   {
     const dialogRef = this.dialog.open(ContactAddFormComponent, {
