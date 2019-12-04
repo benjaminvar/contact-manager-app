13b9512d721c19fb567451937622ad9d95333e9b
Benjamin Vargas <bvj2206@gmail.com>
2019-12-04 03:07:52 -0400

    fetch at least 5 contacts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,15 +1,43 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 import { Contact } from "./contact"
+
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
   contacts: Contact[] = [];
+  private isFetched: boolean = false;
   constructor() { }
+  private fetchRandomContacts(numberOfContacts: number)
+  {
+    return fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(contacts => {
+      let contactsArr: Contact[] = contacts.map(contact => new Contact(contact.name,contact.email,contact.phone))
+      for(let i = 0; i < numberOfContacts; i++)
+      {
+        //Get random index of contact from list of fetched contacts 
+        let randomIndex = Math.floor(Math.random() * (contactsArr.length - 1) );
+        //remove contact from the list of fetch contact and inserted into the contact list
+        let selectedContact= contactsArr.splice(randomIndex,1) ;
+       this.contacts.push(...selectedContact);
+       
+      }
+      this.contacts = contactsArr;
+      return this.contacts;
+    })
+
+  }
   getContacts(): Observable<Contact[]>
   {
+    if(!this.isFetched)
+    {
+      this.isFetched = true;
+      return from(this.fetchRandomContacts(5));
+    }
     return of(this.contacts);
   }
   addContact(contact: Contact): void
